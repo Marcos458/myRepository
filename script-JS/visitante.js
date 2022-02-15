07360b469a373bda1b4c046d511317acc7e9fa48
Marcos <marcossilva7@outlook.com.br>
2022-02-15 01:20:21 -0300

    atualizaçao05 loja

--- script-JS/visitante.js
+++ script-JS/visitante.js
@@ -2,6 +2,6 @@ var data = document.querySelector("#dataHora")
 var a = new Date()
 var Data = (a.toLocaleDateString("pt-BR"))
 var hora = (a.toLocaleTimeString("pt-BR"))
-data.textContent = `${Data} e são ${hora}`
+data.textContent = `${Data} e são ${hora}hs`
 
 
